552a089b277aec375f8436db6abea014d841f7b6
Jordi Sola <someth2say@gmail.com>
2020-07-01 15:38:14 +0200

    feat(plain): Improved tests

--- src/test/java/org/someth2say/yqTest.java
+++ src/test/java/org/someth2say/yqTest.java
@@ -16,29 +16,43 @@ import io.quarkus.test.junit.QuarkusTest;
 public class yqTest {
 
     @Test
-    public void runTestYaml() throws yqException {
+    public void runTestYamlInput() throws yqException {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         System.setOut(new PrintStream(baos));
 
         int exitcode = new yq().run("-f", "src/test/resources/menu.yaml", "-q", "$.menu.popup.menuitem[*].value");
-        assertEquals(0, exitcode, () -> "Basic run failed.");
+        assertEquals(0, exitcode, () -> "Basic YAML run failed.");
 
         List<?> list = new Yaml().stringToObject(baos.toString(), List.class);
                 assertTrue(list.containsAll(List.of("New","Open","Close")),()->"JsonPath results on yaml file unexpected");
     }
 
     @Test
-    public void runTestJson() throws yqException {
+    public void runTestJsonInput() throws yqException {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         System.setOut(new PrintStream(baos));
 
         int exitcode = new yq().run("-f", "src/test/resources/menu.json", "-q", "$.menu.popup.menuitem[*].value");
-        assertEquals(0, exitcode, () -> "Basic run failed.");
+        assertEquals(0, exitcode, () -> "Basic JSON run failed.");
 
         String jsonOutput = baos.toString();
         List<?> list = new Json().stringToObject(jsonOutput, List.class);
         assertTrue(list.containsAll(List.of("New","Open","Close")),()->"JsonPath results on json file unexpected");
     }
+
+    @Test
+    public void runTestPlainInput() throws yqException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(baos));
+
+        int exitcode = new yq().run("-f", "./src/test/resources/menuItems.txt","-i","plain","-o","json");
+        assertEquals(0, exitcode, () -> "Basic PLAIN run failed.");
+
+        String plainOutput = baos.toString();
+        List<?> list = new Json().stringToObject(plainOutput, List.class);
+        assertTrue(list.containsAll(List.of("New","Open","Close")),()->"JsonPath results on json file unexpected");
+    }
+
 
     @Test
     public void runMissingFile() {
@@ -58,9 +72,4 @@ public class yqTest {
         assertNotEquals(0, run, ()->"Unparseable JsonPath should return non-zero");
     }
 
-    @Test
-    public void planOutput() {
-        int run = new yq().run("-f", "src/main/resources/application.properties", "-o","plain");
-        assertEquals(0, run, ()->"Unparseable JsonPath should return non-zero");
-    }
 }
